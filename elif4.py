c8d7eab3df169ded956523ecdde02fbd9b6b7b87
gagangiri94 <gagan@nclouds.com>
2018-04-04 00:43:22 +0530

    new_file

---
+++ elif4.py
@@ -0,0 +1,21 @@
+name = "kamran"
+if name == "Gagan":
+	print("blue color suit")
+elif name == "kamran":
+	print("Black color suit")
+elif name == "Harman":
+	print("Green color suit")
+else:
+	print("no one present")			
+
+
+
+name = "Jason"
+if name == "Gagan":
+	print("blue color suit")
+elif name == "kamran":
+	print("Black color suit")
+elif name == "Harman":
+	print("Green color suit")
+else:
+	print("no one present")
