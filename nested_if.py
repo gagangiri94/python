fa450ce329d4ccc5a1fdd216f8073576d339e561
gagangiri94 <gagan@nclouds.com>
2018-04-04 03:14:18 +0530

    new_file

---
+++ nested_if.py
@@ -0,0 +1,47 @@
+email = "gagangiri94@gmail.com"
+password = "gaganG94"
+
+if email == "gagangiri94@gmil.com":
+	if password == "gagan":
+		print("DONE")
+	else:
+		print("Wrong password")	
+else:
+	print ("ERROR")
+
+
+
+
+
+
+
+
+
+
+email = "gagangiri94@gmail.com"
+password = "gaganG94"
+
+if email == "gagangiri94@gmail.com":
+	if password == "gagan":
+		print("DONE")
+	else:
+		print("Wrong password")	
+else:
+	print ("ERROR")
+
+
+
+
+
+
+email = "gagangiri94@gmail.com"
+password = "gaganG94"
+
+if email == "gagangiri94@gmail.com":
+	if password == "gaganG94":
+		print("DONE")
+	else:
+		print("Wrong password")	
+else:
+	print ("ERROR")
+
